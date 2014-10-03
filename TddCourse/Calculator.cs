d3468d5a9fc4131c665ab8aef853a1d2a225a800
Dariusz Wozniak <dariusz.wozniak@gmail.com>
2014-10-03 17:35:22 +0200

    added calculator class from http://dariuszwozniak.net/2013/07/16/kurs-tdd-czesc-5-nasz-drugi-test-jednostkowy/

---
+++ TddCourse/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddCourse
+{
+    internal interface ICalculator
+    {
+        float Divide(int dividend, int divisor);
+        event EventHandler CalculatedEvent;
+    }
+
+    internal class Calculator : ICalculator
+    {
+        public float Divide(int dividend, int divisor)
+        {
+            if (divisor == 0) throw new DivideByZeroException();
+
+            float result = (float) dividend/divisor;
+            OnCalculated();
+            return result;
+        }
+
+        public event EventHandler CalculatedEvent;
+
+        protected virtual void OnCalculated()
+        {
+            var handler = CalculatedEvent;
+            if (handler != null) handler(this, EventArgs.Empty);
+        }
+    }
+}
