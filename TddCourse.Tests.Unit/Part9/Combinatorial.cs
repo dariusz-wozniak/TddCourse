811f9dec447cf50e8a71a663abaf471fb4ac19f0
Dariusz Wozniak <dariusz.wozniak@gmail.com>
2015-02-09 20:42:39 +0100

    Added combinatorial and sequential tests

---
+++ TddCourse.Tests.Unit/Part9/Combinatorial.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace TddCourse.Tests.Unit.Part9
+{
+    [TestFixture]
+    public class Combinatorial
+    {
+        [Test]
+        [Combinatorial]
+        public void Divide_DividendIsPositiveAndDivisorIsNegative_ReturnsNegativeQuotient(
+            [Values(1, 2, 3, 4)] int dividend,
+            [Values(-1, -2, -3)] int divisor)
+        {
+            var calc = new Calculator();
+            float quotient = calc.Divide(dividend, divisor);
+
+            Assert.That(quotient < 0);
+        }
+    }
+}
