11b41b651f784751b0b3b0d87e193c2004c9ac93
Dariusz Wozniak <dariusz.wozniak@gmail.com>
2015-02-25 20:41:57 +0100

    Added Theories

---
+++ TddCourse.Tests.Unit/Part10/Theory.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace TddCourse.Tests.Unit.Part10
+{
+    public class Theory
+    {
+        [Datapoint]
+        public int Zero = 0;
+
+        [Datapoints]
+        public int Negative = -1;
+        
+        [Datapoint]
+        public int Positive = 1;
+
+        /// <summary>
+        /// positive / negative = negative
+        /// </summary>
+        [Theory]
+        public void WhenDividendIsPositiveAndDivisorIsNegative_TheQuotientIsNegative(int dividend, int divisor)
+        {
+            Assume.That(dividend > 0);
+            Assume.That(divisor < 0);
+
+            var calculator = new Calculator();
+
+            float quotient = calculator.Divide(dividend, divisor);
+
+            Assert.That(quotient < 0);
+        }
+
+        /// <summary>
+        /// 0 / x = 0
+        /// </summary>
+        [Theory]
+        public void WhenDividendIsEqualToZero_TheQuotientIsEqualToZero(int dividend, int divisor)
+        {
+            Assume.That(dividend == 0);
+            Assume.That(divisor != 0);
+
+            var calculator = new Calculator();
+
+            float quotient = calculator.Divide(dividend, divisor);
+
+            Assert.That(quotient == 0);
+        }
+    }
+}
