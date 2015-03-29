406a3caf97dc076dac4c75eb914a445b73f05fb9
Dariusz Wozniak <dariusz.wozniak@gmail.com>
2015-03-29 14:44:19 +0200

    Added Part 11 - Generic Tests

---
+++ TddCourse.Tests.Unit/Part11/GenericCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace TddCourse.Tests.Unit.Part11
+{
+    [TestFixture(typeof(int))]
+    [TestFixture(typeof(float))]
+    [TestFixture(typeof(double))]
+    [TestFixture(typeof(decimal))]
+    public class GenericCalculatorTests<T>
+    {
+        [Test]
+        public void AdditionTest()
+        {
+            var calculator = new GenericCalculator<T>();
+
+            dynamic result = calculator.Add((dynamic) 2, (dynamic) 3);
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+    }
+}
---
+++ TddCourse/GenericCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace TddCourse
+{
+    public class GenericCalculator<T>
+    {
+        public T Add(T a, T b)
+        {
+            return (dynamic) a + (dynamic) b;
+        }
+    }
+}
