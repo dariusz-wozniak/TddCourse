527a4570fe9edba876d285565d426cc7582759f6
Dariusz Woźniak <dariusz.wozniak@sabre.com>
2016-01-03 18:54:43 +0100

    Moved CustomerMock to a separate file

---
+++ TddCourse.Tests.Unit/Part14/CustomerMock.cs
@@ -0,0 +1,14 @@
+﻿namespace TddCourse.Tests.Unit.Part14
+{
+    internal class CustomerMock : ICustomer
+    {
+        private readonly int _expectedAge;
+
+        public CustomerMock(int expectedAge)
+        {
+            _expectedAge = expectedAge;
+        }
+
+        public int GetAge() => _expectedAge;
+    }
+}
--- TddCourse.Tests.Unit/Part14/CustomerValidatorTests.cs
+++ TddCourse.Tests.Unit/Part14/CustomerValidatorTests.cs
@@ -39,16 +39,4 @@ namespace TddCourse.Tests.Unit.Part14
             validate.Should().BeTrue();
         }
     }
-
-    internal class CustomerMock : ICustomer
-    {
-        private readonly int _expectedAge;
-
-        public CustomerMock(int expectedAge)
-        {
-            _expectedAge = expectedAge;
-        }
-
-        public int GetAge() => _expectedAge;
-    }
 }
