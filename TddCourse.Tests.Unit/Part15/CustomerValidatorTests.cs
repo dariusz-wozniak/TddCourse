9ce6692f1d2d0f82569b5e484bfbe2a12cde6f25
Dariusz Wozniak <dariusz.wozniak@gmail.com>
2016-01-09 18:02:45 +0100

    Added Moq tests

---
+++ TddCourse.Tests.Unit/Part15/CustomerValidatorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace TddCourse.Tests.Unit.Part15
+{
+    [TestFixture]
+    public class CustomerValidatorTests
+    {
+        [Test]
+        public void WhenCustomerIsNull_ThenArgumentNullExceptionIsThrown()
+        {
+            var validator = new CustomerValidator();
+
+            Action action = () => validator.Validate(null);
+
+            action.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Test]
+        public void WhenCustomerHasAgeLessThan18_ThenValidationFails()
+        {
+            var validator = new CustomerValidator();
+
+            Mock<ICustomer> customerMock = new Mock<ICustomer>();
+            customerMock.Setup(x => x.GetAge()).Returns(16);
+
+            bool validate = validator.Validate(customerMock.Object);
+
+            validate.Should().BeFalse();
+        }
+
+        [Test]
+        public void WhenCustomerHasAgeGreaterThanOrEqualTo18_ThenValidationPasses([Values(18, 19)] int expectedAge)
+        {
+            var validator = new CustomerValidator();
+
+            Mock<ICustomer> customerMock = new Mock<ICustomer>();
+            customerMock.Setup(x => x.GetAge()).Returns(expectedAge);
+
+            bool validate = validator.Validate(customerMock.Object);
+
+            validate.Should().BeTrue();
+        }
+    }
+}
