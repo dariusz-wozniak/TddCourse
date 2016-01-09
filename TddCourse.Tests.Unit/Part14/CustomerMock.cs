e7f7fd3cb128d4879f575bec7ee7480cd785b8f4
Dariusz Wozniak <dariusz.wozniak@gmail.com>
2016-01-09 19:27:42 +0100

    Updated CustomerMock

--- TddCourse.Tests.Unit/Part14/CustomerMock.cs
+++ TddCourse.Tests.Unit/Part14/CustomerMock.cs
@@ -1,10 +1,14 @@
-﻿namespace TddCourse.Tests.Unit.Part14
+﻿using System.Collections.Generic;
+
+namespace TddCourse.Tests.Unit.Part14
 {
     internal class CustomerMock : ICustomer
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public int PercentageDiscount { get; set; }
         public IPhoneNumber PhoneNumber { get; set; }
+        public IList<IOrder> Orders { get; set; }
 
         private readonly int _expectedAge;
 
