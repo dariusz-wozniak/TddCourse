f105f8a71c81681e0e14f5b9f494addd6df05b88
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2018-12-01 13:39:29 +0100

    Update packages

--- TddCourse.Tests.Unit/Part08/Parameterized_Combined.cs
+++ TddCourse.Tests.Unit/Part08/Parameterized_Combined.cs
@@ -7,8 +7,8 @@ namespace TddCourse.Tests.Unit.Part08
     {
         [Test]
         public void Divide_DividendIsPositiveValue(
-            [Random(min: 1, max: 100, count: 10)] double dividend,
-            [Range(@from: 10, to: 100, step: 10)] double divisor)
+            [Random(min: 1, max: 100, count: 10)] int dividend,
+            [Range(@from: 10, to: 100, step: 10)] int divisor)
         {
             var calc = new Calculator();
             float quotient = calc.Divide(dividend, divisor);
